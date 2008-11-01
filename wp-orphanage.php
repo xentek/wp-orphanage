7c16020f5d356c46bbc2d522042ddb5d9d2c8856
Eric Marden <github@xentek.net>
2008-11-01 20:57:46 +0000

    INITIAL IMPORT
    git-svn-id: https://xentek.unfuddle.com/svn/xentek_wp-orphanage/trunk@4 baed97e3-58fc-4f2f-b1cb-c8a431cfbdf2

---
+++ wp-orphanage.php
@@ -0,0 +1,50 @@
+<?php
+/*
+	Plugin Name: WP-Orphanage
+	Plugin URI: http://xentek.net/code/wordpress/plugins/wp-orphanage/
+	Description: Plugin to promote users with no roles set (the orphans) to the Subscriber role. Their role is upgraded when they login. Orphans are created when using the Shared User Table approach to tying wordpress sites together (and possibly when integrating bbPress into a WP site).
+	Version: 0.4b
+	Author: Eric Marden
+	Author URI: http://xentek.net/
+
+	Copyright 2008  Eric Marden  (email : wp@xentek.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+	add_action('wp_login', 'adopt_this_orphan');
+	add_action('load-users.php', 'adopt_all_orphans');
+
+	function adopt_this_orphan() {
+		global $user_ID;
+		      get_currentuserinfo();
+		
+			if ( !current_user_can('read') ) {
+				$user = new WP_User($user_ID);
+				$user->set_role('subscriber');
+			}
+		}
+		
+		function adopt_all_orphans() {
+			// Query the users
+			$wp_user_search = new WP_User_Search();
+			foreach ( $wp_user_search->get_results() as $userid ) {
+				$user = new WP_User($userid);
+				if ( !$user->has_cap('read') ) {
+					$user->set_role('subscriber');
+				}
+			}
+		}
+?>
